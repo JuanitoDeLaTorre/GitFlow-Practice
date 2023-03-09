aa9e23daceefea5668b7e35191c8ca1815763cd0
joshuapent <joshdavidpent@gmail.com>
2023-03-09 09:26:42 -0600

    Nothing to see here

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/bazinga', (req, res) => {
+  res.render('nonexistant')
+})
+
 module.exports = router;
